70efeefb5ce231b4a48f9cb34cf06c373cb9c20c
Shrey Tailor <shreym.tailor@gmail.com>
2022-04-21 18:07:42 +1200

    [#62] Feature: Implement the WordCloud component (#68)

---
+++ frontend/src/components/WordCloud/WordCloud.tsx
@@ -0,0 +1,38 @@
+import React, { CSSProperties } from 'react';
+import styles from './WordCloud.module.css';
+
+interface WordCloudProps {
+  word: string;
+  size: 'SMALL' | 'LARGE';
+  className?: string;
+}
+
+export default function WordCloud({ word, size, className }: WordCloudProps) {
+  const SMALL_FONT_SIZE = 11;
+  const LARGE_FONT_SIZE = 15;
+  const fontSize = size === 'SMALL' ? SMALL_FONT_SIZE : LARGE_FONT_SIZE;
+
+  const SMALL_X_PADDING_FACTOR = 2;
+  const LARGE_X_PADDING_FACTOR = 3;
+  const xPadding =
+    (size === 'SMALL' ? SMALL_X_PADDING_FACTOR : LARGE_X_PADDING_FACTOR) * word.length;
+
+  const SMALL_Y_PADDING = word.length < 8 ? 20 : 25;
+  const LARGE_Y_PADDING = word.length < 8 ? 25 : 35;
+  const yPadding = size === 'SMALL' ? SMALL_Y_PADDING : LARGE_Y_PADDING;
+
+  const dynamicStyle: CSSProperties = {
+    fontSize: `${fontSize}px`,
+    padding: `${yPadding}px ${xPadding}px`,
+  };
+
+  return (
+    <div className={`${styles.cloud} ${className}`} style={dynamicStyle}>
+      {word}
+    </div>
+  );
+}
+
+WordCloud.defaultProps = {
+  className: '',
+};
